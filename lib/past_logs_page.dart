b8bab4e6a7edf9c7c551debcc6e865806c754754
Charlotte Jacques <charlottej3333@gmail.com>
2025-01-27 21:47:43 -0800

    worked on grouping data

--- lib/past_logs_page.dart
+++ lib/past_logs_page.dart
@@ -18,6 +18,7 @@ class _PastLogsPageState extends State<PastLogsPage> {
 
   DatabaseReference dbRef = FirebaseDatabase.instance.ref().child('GratitudeLogs');
   List<Map<dynamic, dynamic>> gratitudeLogs = [];
+  Map<String, List<String>> categorizedLogs = {};
   bool loading = true;
 
   @override
@@ -26,57 +27,60 @@ class _PastLogsPageState extends State<PastLogsPage> {
     print('initially mounted: $mounted');
     
     dbRef.orderByChild('date').onValue.listen((event) {
-      // if (mounted) {
-        print('we here');
-        //re-initialize gratitudeLogs to empty
-        gratitudeLogs = [];
+      //re-initialize gratitudeLogs to empty
+      gratitudeLogs = [];
 
-        DataSnapshot dataSnapshot = event.snapshot;
-        Map<dynamic, dynamic> values = dataSnapshot.value as Map<dynamic, dynamic>;
-        // print(values);
-        values.forEach((key, value) {
-          // print('Key: $key');
-          // print('Log: ${value['gratitude_item']}');
-          // print('Date: ${value['date']}');
-          
-          // add to gratitude logs + sort
-          if (mounted) {
-            print('mounted: $mounted');
-            try {
-              setState(() {
-                gratitudeLogs.add(value);
-                loading = false;
-            });
-            } catch (e) {
-              print('error with setState $e');
-            }
+      DataSnapshot dataSnapshot = event.snapshot;
+      Map<dynamic, dynamic> values = dataSnapshot.value as Map<dynamic, dynamic>;
+      values.forEach((key, value) {
+        
+        // add to gratitude logs + sort
+        if (mounted) {
+          try {
+            setState(() {
+              gratitudeLogs.add(value);
+              loading = false;
+          });
+          } catch (e) {
+            print('error with setState $e');
           }
-          
-        });
-
-        //sort by date
-        gratitudeLogs.sort((a, b) => a['date'].compareTo(b['date']));
-
-        //group by date
-
-        /** ideally: map with key being a date (in sorted order), value being a list of logs */
-        for (final item in gratitudeLogs) {
-          DateTime date = DateTime.parse(item['date']);
-          String formatted;
-          int daysAgo = calculateDifference(date);
-
-          if (daysAgo == 0) {
-            formatted = 'Today';
-          } else if (daysAgo == 1) {
-            formatted = 'Yesterday';
-          } else if (daysAgo <= 6){
-            formatted = DateFormat('EEEE', 'en_US').format(date);
-          } else {
-            formatted = DateFormat.yMMMMEEEEd().format(date);
-          }
-          item['date'] = formatted;
         }
-      // }
+        
+      });
+
+      //sort by date
+      gratitudeLogs.sort((a, b) => a['date'].compareTo(b['date']));
+
+      //group by date
+
+      /** ideally: map with key being a date (in sorted order), value being a list of logs */
+      for (final item in gratitudeLogs) {
+        DateTime date = DateTime.parse(item['date']);
+        String formatted;
+        int daysAgo = calculateDifference(date);
+
+        if (daysAgo == 0) {
+          formatted = 'Today';
+        } else if (daysAgo == 1) {
+          formatted = 'Yesterday';
+        } else if (daysAgo <= 6){
+          formatted = DateFormat('EEEE', 'en_US').format(date);
+        } else {
+          formatted = DateFormat.yMMMMEEEEd().format(date);
+        }
+        item['date'] = formatted;
+
+        if (mounted) {
+          setState(() {
+            if (categorizedLogs.containsKey(formatted)) {
+              categorizedLogs[formatted]!.add(item['gratitude_item']);
+            } else {
+              categorizedLogs[formatted] = [item['gratitude_item']];
+            }
+          });
+        }
+      }
+      print(categorizedLogs);
     });
   }
 
@@ -91,32 +95,60 @@ class _PastLogsPageState extends State<PastLogsPage> {
 
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Text(
-              'Past Logs',
-              style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                color: Theme.of(context).colorScheme.primary,
+
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Past Logs',
+                style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
-            ),
-            loading //display "loading text while loading"
-              ? CircularProgressIndicator()
-            : Expanded( //otherwise, display list of logs
-              child: gratitudeLogs.isNotEmpty
-                ? ListView.builder(
-                  itemCount: gratitudeLogs.length,
-                  prototypeItem: Text(
-                    gratitudeLogs.first['date'] + ': ' + gratitudeLogs.first['gratitude_item']
-                  ),
-                  itemBuilder: (context, index) {
-                    return Text(
-                      gratitudeLogs[index]['date'] + ': ' + gratitudeLogs[index]['gratitude_item']
-                    );
-                  },
-                )
-                : Center(child: Text('No gratitude logs')),
-            )
-          ],
+              // loading //display "loading text while loading"
+                // ? CircularProgressIndicator()
+              // : Expanded( //otherwise, display list of logs
+              //   child: gratitudeLogs.isNotEmpty
+                  // ? ListView.builder(
+                  //   itemCount: gratitudeLogs.length,
+                  //   prototypeItem: Text(
+                  //     gratitudeLogs.first['date'] + ': ' + gratitudeLogs.first['gratitude_item']
+                  //   ),
+                  //   itemBuilder: (context, index) {
+                  //     return Text(
+                  //       gratitudeLogs[index]['date'] + ': ' + gratitudeLogs[index]['gratitude_item']
+                  //     );
+                  //   },
+                  // )
+                  ListView.builder(
+                    itemCount: categorizedLogs.length,
+                    prototypeItem: Text('Hey'),
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      List<String> lst = categorizedLogs.values.elementAt(index);
+                      return ListView(
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(categorizedLogs.keys.elementAt(index)),
+                          ListView.builder(
+                            itemCount: lst.length,
+                            prototypeItem: Text('fdsj'),
+                            itemBuilder: (context, index) {
+                              return Text(lst[index]);
+                            },
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                          )
+                        ],
+                      );
+                    },
+                  )
+                  // : Center(child: Text('No gratitude logs')),
+              // )
+            ],
+          ),
         ),
       ),
     );
