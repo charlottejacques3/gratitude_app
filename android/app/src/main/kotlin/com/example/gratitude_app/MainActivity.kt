88965487d684841b21f59b17c68bfd71059b8b0f
Charlotte Jacques <charlottej3333@gmail.com>
2025-02-24 21:10:59 -0800

    tried some more things

--- android/app/src/main/kotlin/com/example/gratitude_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gratitude_app/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.example.gratitude_app
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+
+import io.flutter.embedding.android.FlutterActivity
+import dev.fluttercommunity.plus.androidalarmmanager.AndroidAlarmManagerPlugin
+
+class MainActivity: FlutterActivity() {
+    // override fun onCreate(savedInstanceState: Bundle?) {
+    //     super.onCreate(savedInstanceState)
+    //     AndroidAlarmManagerPlugin.registerWith(flutterEngine!!)
+    // }
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)  // Registers plugins automatically
+    }
+}
+
