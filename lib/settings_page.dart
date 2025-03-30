c3fc75d3e7622ed0c3f797e32829b04f94d965ca
Charlotte Jacques <charlottej3333@gmail.com>
2025-03-30 12:55:29 -0700

    updated

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -2,7 +2,7 @@
   import 'package:firebase_auth/firebase_auth.dart';
   import 'package:flutter/material.dart';
   import 'package:flutter/services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+  import 'package:shared_preferences/shared_preferences.dart';
 
   //import files
   import 'utilities/helper_functions.dart';
