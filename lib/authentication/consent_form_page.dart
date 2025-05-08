204ff99cd900c214fb6a1ec616509f2a947a09ee
Charlotte Jacques <charlottej3333@gmail.com>
2025-05-08 16:34:25 -0700

    added e-signature

--- lib/authentication/consent_form_page.dart
+++ lib/authentication/consent_form_page.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:scroll_screenshot/scroll_screenshot.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:signature/signature.dart';
 
 
 class ConsentFormPage extends StatefulWidget {
@@ -23,7 +26,7 @@ class _ConsentFormPageState extends State<ConsentFormPage> {
   final formKey = GlobalKey<FormState>();
   final screenshotKey = GlobalKey();
 
-  Uint8List imageDecode = Uint8List(0);
+  // Image imageDecode = Image(image);// = Uint8List(0);
 
   bool readForm = false;
   bool askQuestions = false; 
@@ -32,43 +35,69 @@ class _ConsentFormPageState extends State<ConsentFormPage> {
   bool consent = false;
   TextEditingController name = TextEditingController();
   TextEditingController date = TextEditingController();
+  SignatureController signatureController = SignatureController(); //can set pen colour, etc.
+  Uint8List? signatureBytes;// = Uint8List(0);
+
+  double? formHeight;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    //get form height
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final RenderBox box = screenshotKey.currentContext!.findRenderObject() as RenderBox;
+      setState(() {
+        formHeight = box.size.height;
+      });
+    });
+  }
+
 
   //generate pdf from consent form data
-  void generatePdf() async {
+  void generatePdf(Uint8List imgBytes) async {
+    final imgProvider = pw.MemoryImage(imgBytes);
     final pdf = pw.Document();
 
     pdf.addPage(
       pw.Page(
-        build: (pw.Context context) {
-          return pw.Column(
-            children: [
-              pw.Text(
-                'Dynamic Scaffolding Gratitude Application Study',
-                style: pw.TextStyle(
-                  fontSize: 20,
-                  fontWeight: pw.FontWeight.bold
-                ),
-                textAlign: pw.TextAlign.center
-              ),
-              pw.Text('CONSENT FORM STUFF',
-              ),
-              pw.Text('Please remember that participation in this study is voluntary.',
-                style: pw.TextStyle(
-                  fontWeight: pw.FontWeight.bold
-                ),
-              ),
-
-              //radio buttons
-              pw.Text('Label'),
-              pw.Row(
-              ),
-
-              pw.Text('Name: ${name.text}')
-            ]
-          );
-        }
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) => pw.Center(child: pw.Image(imgProvider))
       )
     );
+    // pdf.addPage(
+    //   pw.Page(
+    //     build: (pw.Context context) {
+    //       return pw.Column(
+    //         children: [
+    //           pw.Text(
+    //             'Dynamic Scaffolding Gratitude Application Study',
+    //             style: pw.TextStyle(
+    //               fontSize: 20,
+    //               fontWeight: pw.FontWeight.bold
+    //             ),
+    //             textAlign: pw.TextAlign.center
+    //           ),
+    //           pw.Text('CONSENT FORM STUFF',
+    //           ),
+    //           pw.Text('Please remember that participation in this study is voluntary.',
+    //             style: pw.TextStyle(
+    //               fontWeight: pw.FontWeight.bold
+    //             ),
+    //           ),
+
+    //           //radio buttons
+    //           pw.Text('Label'),
+    //           pw.Row(
+    //           ),
+
+    //           pw.Text('Name: ${name.text}')
+    //         ]
+    //       );
+    //     }
+    //   )
+    // );
     Uint8List pdfBytes = Uint8List(0);
 
     //save pdf to cloud storage
@@ -217,7 +246,50 @@ class _ConsentFormPageState extends State<ConsentFormPage> {
                     ),
                   ],
                 ),
-            
+
+                //e-signature
+                ElevatedButton(
+                  child: Text('Sign Electronically'),
+                  onPressed: () => showDialog(
+                    context: context, 
+                    builder: (BuildContext context) => Dialog(
+                      child: Column(
+                        children: [
+                          //signing box
+                          Signature(
+                            controller: signatureController,
+                            width: 300,
+                            height: 300,
+                          ),
+
+                          //save and clear
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () => signatureController.clear(),
+                                child: Text('Clear')
+                              ),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  final bytes = await signatureController.toPngBytes();
+                                  setState(() {
+                                    signatureBytes = bytes;
+                                  });
+                                  Navigator.pop(context);
+                                }, 
+                                child: Text('Save')
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ),
+                ),
+
+                //show e-signature
+                signatureBytes != null ? Image.memory(signatureBytes!) : Container(),
+                
             
                 //finish consent form
                 ElevatedButton(
@@ -245,14 +317,27 @@ class _ConsentFormPageState extends State<ConsentFormPage> {
                       //   print('THEIR EMAIL: ${FirebaseAuth.instance.currentUser!.email}');
                       // } 
 
-                      generatePdf();
-                      print('AFTER GENERATING PDF');
+                      //take screenshot
+                      if (formHeight != null) {
+                        final recorder = PictureRecorder();
+                        final canvas = Canvas(recorder);
+                        final size = Size(MediaQuery.of(context).size.width, formHeight!);
+
+                        final repaintBoundary = screenshotKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+                        final image = await repaintBoundary.toImage(pixelRatio: 3.0);
+                        final byteData = await image.toByteData(format: ImageByteFormat.png);
+                        final pngBytes = byteData!.buffer.asUint8List();
+                        
+
+                        generatePdf(pngBytes);
+                        print('AFTER GENERATING PDF');
+                      }
                       
                     }
                   },
                 ),
 
-                imageDecode.isNotEmpty ? Image.memory(imageDecode) : Container()
+                // imageDecode.isNotEmpty ? Image.memory(imageDecode) : Container()
               ],
             ),
           ),
